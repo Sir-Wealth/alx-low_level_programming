945ad844ac3d70390bb915c07665ca1207119a0a
Sir-Wealth <sirwealthessien@gmail.com>
2023-08-10 20:49:29 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program with printf function
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return(0)
+}
