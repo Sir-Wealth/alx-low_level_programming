8503e0039ac48821f72a70343ece5c0b6cd493c1
Sir-Wealth <sirwealthessien@gmail.com>
2023-08-11 16:12:15 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ * Return: eachtime 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
+
