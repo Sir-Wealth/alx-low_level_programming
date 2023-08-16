1ef109f508d49eb0d664c36e1f51b6b528b5ec7e
Sir-Wealth <sirwealthessien@gmail.com>
2023-08-16 04:34:47 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet
+ * print lowercase alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for(n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+	_putchar('\n');
+}
