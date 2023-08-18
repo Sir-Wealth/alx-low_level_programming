ac3d12a74b594e13ab10b8beb9f618b0c5ed08f4
Sir-Wealth <sirwealthessien@gmail.com>
2023-08-18 02:41:51 -0700

    task 0

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -27,18 +27,22 @@ int convert_day(int month, int day)
 		case 6:
 			day = 151 + day;
 			break;
+	}
 		case 7:
 			day = 181 + day;
 			break;
 		case 8:
 			day = 212 + day;
 			break;
+	else
 		case 9:
 			day = 243 + day;
 			break;
 		case 10:
 			day = 273 + day;
 			break;
+	}
+
 		case 11:
 			day = 304 + day;
 			break;
