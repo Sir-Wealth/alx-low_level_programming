8e03277d71fa043d43e36a61838ac8f5319dbad6
Sir-Wealth <sirwealthessien@gmail.com>
2023-08-24 10:14:16 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+
+char *leet(char *n)
+{
+	int v, w;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (v = 0; n[v] != '\0'; v++)
+	{
+		for (w = 0; w < 10; w++)
+		{
+			if (n[v] == s1[w])
+			{
+				n[v] = s2[w];
+			}
+		}
+	}
+	return (n);
+}
